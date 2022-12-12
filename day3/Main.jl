6fb9e8d8795de3d4780843e20d68547d5220a0ad
dmgcodevil <dmgcodevil@gmail.com>
2022-12-12 02:22:12 -0500

    day3-2

--- day3/Main.jl
+++ day3/Main.jl
@@ -1,3 +1,12 @@
+
+function getscore(ch::Char)::Int
+    if islowercase(ch)
+        ch - 'a' + 1
+    else
+        ch - 'A' + 27
+    end
+end
+
 function part1()
     res = 0
     open("input.txt") do f
@@ -8,15 +17,25 @@ function part1()
             b = collect(Set(s[mid+1:end]))
             errors = ∩(a, b)
             for ch in errors
-                if islowercase(ch)
-                    res += ch - 'a' + 1
-                else
-                    res += ch - 'A' + 27
-                end
+                res += getscore(ch)
             end
         end
     end
     println("answer: " * string(res))
 end
 
-part1()
+function part2()
+    res = 0
+    open("input.txt") do f
+        while !eof(f)
+            a = Set(collect(readline(f)))
+            b = Set(collect(readline(f)))
+            c = Set(collect(readline(f)))
+            res += getscore(first(∩(∩(a, b), c)))
+        end
+    end
+    println("answer: " * string(res))
+end
+
+#  part1()
+part2()
